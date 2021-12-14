ac026fae923bcff9b90a053cc472acdd4562a0d2
David-Elkabas <david.elkabass@gmail.com>
2021-12-14 14:12:30 +0200

    add update and delete operations

--- server/routes/api/todos.js
+++ server/routes/api/todos.js
@@ -23,16 +23,49 @@ router.get("/:id", (req, res) => {
 // CREATE one todo
 router.post("/", (req, res) => {
   const newTodo = {
-    id: uuid.v4(),
+    id: uuid.v4(), //create a random id
     title: req.body.title,
     completed: false,
   };
   if (!newTodo.title) {
     return res.status(400).json({ message: "please include a title" });
   }
-
   todos.push(newTodo);
   res.json(todos);
+});
+
+// UPDATE one todo
+router.put("/:id", (req, res) => {
+  const isFound = todos.some((todo) => todo.id === parseInt(req.params.id));
+  if (isFound) {
+    const updateTodo = req.body;
+    todos.forEach((todo) => {
+      if (todo.id === parseInt(req.params.id)) {
+        todo.title = updateTodo.title ? updateTodo.title : todo.title;
+        todo.completed = updateTodo.completed
+          ? updateTodo.completed
+          : todo.completed;
+        res.json({ message: "todo updated", todo });
+      }
+    });
+  } else {
+    res
+      .status(400)
+      .json({ message: `no todo with id of ${req.params.id} was found` });
+  }
+});
+
+// DELETE a single todo
+router.delete("/:id", (req, res) => {
+  const isFound = todos.some((todo) => todo.id === parseInt(req.params.id));
+  if (isFound) {
+    newTodos = todos.filter((todo) => todo.id !== parseInt(req.params.id));
+    res.json({ message: "todo deleted", todos: newTodos });
+  } else {
+    res
+      .status(400)
+      .json({ message: `no todo with id of ${req.params.id} was found` });
+  }
 });
 
 module.exports = router;
